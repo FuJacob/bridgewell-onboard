c03a616d3506bd7cc195798d5030c161391a0e5f
Jacob Fu <jjacobfu@gmail.com>
2025-07-27 23:26:51 -0400

    as

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,9 +4,13 @@ import Landing from "@/components/pages/Landing";
 export default function Home() {
   return (
     <>
-      <Nav />
+      This site is under maintenance.
+      We hope to bring you a bug-free experience soon. 
+      Please email Jordan if you have any questions. Hopefully we can have it out August 4th, 2025.
+      Thank you for your patience.
+      {/* <Nav />
       <Landing />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
